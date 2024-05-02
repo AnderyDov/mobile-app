6b26d0414fa3105e8f8ca526e3829144095d6ff5
AndreyDov <dovban.andreyy@rambler.ru>
2024-05-02 07:33:06 +0300

    init: Начало проекта

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, Image, StyleSheet } from 'react-native';
+import { Input } from './shared';
+import palette from './palette';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.content}>
+				<Image style={styles.logo} source={require('./assets/logo.png')} resizeMode="contain" />
+				<View style={styles.form}>
+					<Input />
+					<Input />
+					<Button title="Войти" />
+				</View>
+				<Text style={styles.link}>Восстановить пароль</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		justifyContent: 'center',
+		flexGrow: 1,
+		backgroundColor: '#000000',
+		padding: 55,
+	},
+
+	content: {
+		gap: 50,
+		alignItems: 'center',
+	},
+	logo: {
+		width: 220,
+	},
+	form: {
+		gap: 16,
+		alignSelf: 'stretch',
+	},
+	input: {
+		backgroundColor: palette.grey_stroke,
+	},
+
+	link: {
+		color: palette.white,
+	},
 });
---
+++ palette.jsx
@@ -0,0 +1,15 @@
+export default palete = {
+	black: '#16171D',
+	white: '#FAFAFA',
+	red: '#CC384E',
+	primary: '#6C38CC',
+	primary_hover: '#452481',
+	violet: '#6C38CC',
+	violet_dark: '#2E2D3D',
+	purple: '#D77CE6',
+	links: '#A97BFF',
+	pink: '#E47AD5',
+	grey: '#AFB2BF',
+	grey_stroke: '#4D5064',
+	grey_light: '#DAD9DE',
+};
